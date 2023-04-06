53fdd41ad42763edbe3ec20827ef8b6e0f47120d
Pankratov Semen <68625468+7enSmile@users.noreply.github.com>
2023-04-06 10:55:16 +0000

    fix bug

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from prometheus_client import start_http_server, Gauge
 import torquepy
 import json
 import time
+list_name_of_queue = []
 count_of_queue = Gauge('torque_count_of_queue', 'Description of gauge', ['name'])
 count_of_task = Gauge('torque_count_of_tasks', 'Description of gauge', ['type'])
 count_of_all_task = Gauge('torque_count_of_tasks_all', 'Description of gauge')
@@ -18,6 +19,12 @@ if __name__ == '__main__':
         count_of_task.labels("exit").set(torquepy.TorqueManager.get_count_of_ext_tasks())
         count_of_task.labels("complete").set(torquepy.TorqueManager.get_count_of_com_tasks())
         list_of_queue = torquepy.TorqueManager.get_list_of_queue()
+        if len(list_of_queue) < len(list_name_of_queue):
+            for queue in list_of_queue:
+                if not(queue in list_name_of_queue):
+                    count_of_queue.clear()
+        list_name_of_queue.clear()
         for queue in list_of_queue:
+            list_name_of_queue.append(queue["name"])
             count_of_queue.labels(queue["name"]).set(queue["tasks"])
         time.sleep(1)
